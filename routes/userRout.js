6be854d827f0dd3f4746bb6737116fbebafb249e
shimi bukshpan <buki8060@gmail.com>
2022-08-08 10:42:58 +0300

    chalk

--- routes/userRout.js
+++ routes/userRout.js
@@ -5,35 +5,40 @@ const UserModel = require('../models/usersMongoSchema');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const _ = require('lodash');
+const chalk = require ('chalk');
 
 
 router.post("/create" ,createRequest);
 
 async function createRequest(req, res) {
-
         const { error, value } = userSchema.newUser.validate(req.body);
         const user = value;
         if (error) {
             res.status(400).send(error);
+            console.log(chalk.red(error));
         }
         else {
             try {
                 const result = await UserModel.find({email:user.email});
                 if (result.length > 0) {
                     res.status(400).send("User already exists")
+                    console.log(chalk.red(res.status));
                 }
                 else {
                     try {
                      const savedUser = await saveUser(user);
                      res.status(201).send(savedUser);
+                     console.log(chalk.green(saveUser));
                     }
                     catch (err) {
                         res.status(400).send(err);
+                        console.log(chalk.red(err));
                     }
                     
                 }
             } catch (err) {
                 res.status(400).send(err);
+                console.log(chalk.red(err));
             }
         
         }
@@ -58,6 +63,7 @@ function saveUser(user){
         const user = value;
         if (error) {
             res.status(400).send(error);
+            console.log(chalk.red(error));
         }
         else{
             try{
@@ -68,8 +74,10 @@ function saveUser(user){
                 }
                 const isAuth = await userModel.checkPassword(user.password);
                 res.status(200).send(isAuth);
+                console.log(chalk.green(isAuth));
             } catch (err) {
                 res.status(400).send(err);
+                console.log(chalk.red(err));
             }
         }
     };
